bc8b64407d526460a203725b2c5a4a8e3ade4631
Shogath <shogath@live.com>
2020-04-16 23:28:19 +0430

    Create requirements.txt and making visualservice.py pep8 compatible.

--- mms/visualservice.py
+++ mms/visualservice.py
@@ -9,7 +9,7 @@ import io
 import json
 import pickle
 import logging
-import os 
+import os
 
 # External dependencies
 import hnswlib
@@ -19,7 +19,7 @@ ctx = mx.cpu()
 
 # Fixed parameters
 SIZE = (224, 224)
-MEAN_IMAGE= mx.nd.array([0.485, 0.456, 0.406])
+MEAN_IMAGE = mx.nd.array([0.485, 0.456, 0.406])
 STD_IMAGE = mx.nd.array([0.229, 0.224, 0.225])
 EMBEDDING_SIZE = 512
 INDEX_URL = 'https://s3.us-east-2.amazonaws.com/mxnet-public/visual_search/index.idx'
@@ -28,25 +28,27 @@ ASIN_DATA_URL = 'https://s3.us-east-2.amazonaws.com/mxnet-public/visual_search/A
 EF = 300
 K = 25
 
+
 # Data Transform
 def transform(image):
     resized = mx.image.resize_short(image, SIZE[0]).astype('float32')
     cropped, crop_info = mx.image.center_crop(resized, SIZE)
     cropped /= 255.
     normalized = mx.image.color_normalize(cropped,
-                                      mean=MEAN_IMAGE,
-                                      std=STD_IMAGE) 
-    transposed = nd.transpose(normalized, (2,0,1))
+                                          mean=MEAN_IMAGE,
+                                          std=STD_IMAGE)
+    transposed = nd.transpose(normalized, (2, 0, 1))
     return transposed
 
+
 class VisualSearchService(MXNetBaseService):
-    
+
     def __init__(self, model_name, model_dir, manifest, gpu=None):
         super(VisualSearchService, self).__init__(model_name, model_dir, manifest, gpu)
-        
+
         ############################################
         logging.info('Downloading Resources Files')
-        
+
         data_dir = os.environ.get('DATA_DIR', '/data/visualsearch/mms/')
         if not os.path.isdir(data_dir):
             os.makedirs(data_dir)
@@ -58,22 +60,21 @@ class VisualSearchService(MXNetBaseService):
         mx.test_utils.download(idx_to_ASIN_url, dirname=data_dir)
         mx.test_utils.download(ASIN_to_data_url, dirname=data_dir)
         ############################################
-        
+
         ############################################
         logging.info('Loading Resources files')
-        
+
         self.idx_ASIN = pickle.load(open(os.path.join(data_dir, 'idx_ASIN.pkl'), 'rb'))
-        self.ASIN_data = pickle.load(open(os.path.join(data_dir,'ASIN_data.pkl'), 'rb'))        
-        self.p = hnswlib.Index(space = 'l2', dim = EMBEDDING_SIZE)
-        self.p.load_index(os.path.join(data_dir,'index.idx'))
+        self.ASIN_data = pickle.load(open(os.path.join(data_dir, 'ASIN_data.pkl'), 'rb'))
+        self.p = hnswlib.Index(space='l2', dim=EMBEDDING_SIZE)
+        self.p.load_index(os.path.join(data_dir, 'index.idx'))
         ############################################
-        
+
         logging.info('Resources files loaded')
-        
-        
-        self.p.set_ef(EF)        
+
+        self.p.set_ef(EF)
         self.k = K
-        
+
     def _preprocess(self, data):
         image_b64 = data[0][0]
         image_b64 = image_b64[22:] if image_b64[:4] == 'data' else image_b64
@@ -83,9 +84,9 @@ class VisualSearchService(MXNetBaseService):
         image_t = transform(nd.array(image_np[:, :, :3]))
         image_batchified = image_t.expand_dims(axis=0).as_in_context(ctx)
         return [image_batchified]
-    
+
     def _postprocess(self, data):
-        labels, distances = self.p.knn_query([data[0].asnumpy().reshape(-1,)], k = self.k)
+        labels, distances = self.p.knn_query([data[0].asnumpy().reshape(-1, )], k=self.k)
         logging.info(labels)
         output = []
         for label in labels[0]:
